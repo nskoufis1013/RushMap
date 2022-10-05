4819df2b817a9e6345d58246542aded139db0c65
Nicholas Skoufis <nskoufisphs@gmail.com>
2022-10-05 03:14:32 -0500

    MAPS + ALGO + PRINT TO FRONT

--- app.js
+++ app.js
@@ -1,52 +1,430 @@
-//import {Client} from "@googlemaps/google-maps-services-js";
+console.log("hello there");
+
 
 const GoogleDistanceApi = require('google-distance-api');
-const locations = ['3435 Amanda Ave, Nashville TN 37215', '1615 Woodmont Blvd Nashville, TN 37215', '1700 Woodmont Blvd Nashville, TN 37215', '1715 Woodmont Blvd Nashville, TN 37215'];
+const nodes = ['232 Hillwood Blvd, Nashville, TN 37205', '224 W Hillwood Dr, Nashville, TN 37205', '215 Hillwood Blvd, Nashville, TN 37205', '913 Neartop Dr, Nashville, TN 37205',
+'224 Wilsonia Ave, Nashville, TN 37205', '305 Hillwood Blvd, Nashville, TN 37205', '400 Wilsonia Ave, Nashville, TN 37205'];
 const options = {
   key: 'AIzaSyCWW5D0l2MdXfBzw4kZkq4oCuEeyCvxb8E',
-  origins: locations,
+  origins: nodes,
   //origins: [],
 
   //origins: ['-86.800949,36.14405', 'Nashville, U'],
-  destinations: locations,
+  destinations: nodes,
   mode: 'walking'
 }
 
 
-const data  = GoogleDistanceApi.distance(options, (err, data) => {
-    if(err) {
-        return console.log(err);
-    }
+var costMatrix = new Array(nodes.length);
+//var nodes = locations;
 
-    console.log(data[0].durationValue);
+const express = require('express')
+const app = express()
+const port = 3000
 
-    var count = 0;
-    for (var i = 0; i < locations.length; i++){
-      var tmp = new Array(locations.length);
-      for (var j = 0; j < locations.length; j++){
-        //console.log(costMatrix);
-        tmp[j] = data[count].durationValue;
-        //console.log(data[count].durationValue);
-        //costMatrix[i][j] = data[count].durationValue;
-        count++;
+
+
+
+run();
+
+function run(){
+
+    const data  = GoogleDistanceApi.distance(options, (err, data) => {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log(data[0].durationValue);
+
+        var count = 0;
+        for (var i = 0; i < nodes.length; i++){
+        var tmp = new Array(nodes.length);
+        for (var j = 0; j < nodes.length; j++){
+            //console.log(costMatrix);
+            tmp[j] = data[count].durationValue;
+            //console.log(data[count].durationValue);
+            //costMatrix[i][j] = data[count].durationValue;
+            count++;
+            //console.log(tmp);
+        }
         //console.log(tmp);
-      }
-      //console.log(tmp);
-      costMatrix[i] = tmp;
-      //console.log(costMatrix);
-    }
-    //console.log(costMatrix);
+        costMatrix[i] = tmp;
+        //console.log(costMatrix);
+        }
+        
+        console.log(costMatrix);
+        
+        var cycles = createCycles();
+        var cyclesMatrix = Array(cycles.length);
+        for (var i = 0; i < cycles.length; i++){
+            cycles[i].logCycle();
+            cyclesMatrix[i] = cycles[i].toArray();
 
-    other();
-});
+        }
+        
+        exp(cyclesMatrix);
+    });
 
-function other(){
-  console.log(costMatrix);
+
 }
 
-var costMatrix = new Array(locations.length);
+console.log("hello there");
+
+const MAX_COST = 500;
+
+//console log stuff or not
+const LOG = false;
 
 
-console.log("outside");
-console.log(costMatrix);
-//console.log(data);
+// var nodes = ['232 Hillwood Blvd, Nashville, TN 37205', '224 W Hillwood Dr, Nashville, TN 37205', '215 Hillwood Blvd, Nashville, TN 37205', '913 Neartop Dr, Nashville, TN 37205',
+// '224 Wilsonia Ave, Nashville, TN 37205', '305 Hillwood Blvd, Nashville, TN 37205', '400 Wilsonia Ave, Nashville, TN 37205'];
+
+
+// var costMatrix = [
+    
+//         [
+//             0, 121, 233,
+//           317, 245, 216,
+//           333
+//         ],
+//         [
+//           121,   0, 304,
+//           408, 124, 284,
+//           212
+//         ],
+//         [
+//           274, 351,   0,
+//           350, 475, 476,
+//           559
+//         ],
+//         [
+//           330, 425, 333,
+//             0, 548, 546,
+//           637
+//         ],
+//         [
+//           252, 131, 435,
+//           539,   0, 416,
+//           201
+//         ],
+//         [
+//           160, 226, 379,
+//           476, 350,   0,
+//           438
+//         ],
+//         [
+//           307, 186, 490,
+//           593, 167, 470,
+//             0
+//         ]
+      
+// ];
+
+
+
+//array representing which nodes are already in a cycle; filled with false
+
+
+var visited = Array(nodes.length).fill(false);
+
+var unvisited = nodes.length;
+
+class CycleNode{
+    constructor(element){
+        this.element = element;
+        this.next = null;
+    }
+}
+
+//Data strcutre to store the cycle - implemented as linked list
+class Cycle{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+        this.totalCost = 0;
+
+    }
+
+    //only use this for starting node
+    add(element){
+        var node = new CycleNode(element);
+
+        if (this.head == null){
+            this.head = node;
+            this.tail = node;
+        } else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+
+        //note: cost should be 0 
+    }
+
+    //inserts node containing element after node containing target
+    insert(target, element){
+        var traverse = this.head;
+        while(traverse.element != target){
+            
+            if (traverse.next == null){
+                console.log("**heck** bad insert - target not found");
+                return;
+            }
+
+            traverse = traverse.next;
+        }
+
+        var tmp = traverse.next;
+        var newNode = new CycleNode(element);
+        traverse.next = newNode;
+        newNode.next = tmp;
+
+        this.length++;
+        this.totalCost += cost(traverse.element, newNode.element) + cost(newNode.element, tmp.element) - cost(traverse.element, tmp.element);
+
+
+    }
+
+    getCost(){
+        return this.totalCost;
+    }
+
+    getLength(){
+        return this.length;
+    }
+
+    toArray(){
+        var array = Array(this.length);
+        var tmp = this.head;
+        for (var i = 0; i < this.length; i++){
+            array[i] = nodes[tmp.element];
+            tmp = tmp.next;
+        }
+        return array;
+    }
+
+    logCycle(){
+        console.log(this.toArray());
+        console.log(this.totalCost);
+    }
+}
+
+
+//find the cost between two nodes
+function cost(node1I, node2I){
+    return costMatrix[node1I][node2I];
+}
+
+//marks the node at nodeI as visited 
+function markVisited(nodeI){
+    visited[nodeI] = true;
+    unvisited--;
+}
+
+//fuction to find a maximal cycle containing node at startNodeI
+/**
+     * ALGORITHM - 
+     * let rc be the running cost of the cycle
+     * let c(X,Y) be the cost between two neighbor nodes X, Y
+     * 
+     * Note that nodes are indices. these indicies correspond to the costMatrix
+     * 
+     * start at node A
+     * let the cycle be a list of nodes. The first and last element are A
+     * find A's closest neighbor B
+     * add B to the cycle (between A and A) and rc = 2*c(A,B)    (cost there and back)
+     * mark B as visited
+     * create a list Lcosts. this list will hold the minimum cost of adding the node corresponding to the index into the cycle. initialize to all Infinity
+     * create a list Linsert. each index (corresponding to a node) holds the pair of nodes in the cycle where the new node would be inserted. Initialize to all [A,B]
+     * 
+     * consider every node X besides A and B
+     * - calculate cost = c(A,X) + c(X, B) - c(A,B)
+     * - lcosts[X] = cost
+     * - linsert[X] = [A,B]
+     * - keep track of the X associated with the smallest cost. call this Xmin
+     * 
+     * if rc + lcosts[Xmin] <= MAX_COST
+     * - insert linsert[Xmin] into the cycle between A and B
+     * - rc = rc + lcost[Xmin]
+     * - set linsert[Xmin] to null 
+     * - mark Xmin as visited
+     * 
+     * else terminate: return cycle
+     * 
+     * Function(A, B):
+     * take each node Y  (if linsert[Y] is not null)
+     * - calculate cost1 = c(A,Y) + c(Y,X) - c(A,X), associate this cost with Y inserted between A and X
+     * - calculate cost2 = c(X,Y) + c(Y,B) - c(X,B), associate this cost with Y inserted between X and B
+     * - lcosts[Y] = min(lcosts[Y], cost1, cost2)
+     * - if cost1 <= cost2 && cost1 <= lcost[Y]
+     * - - lcost[Y] = cost1
+     * - - linsert[Y] = [A,X]
+     * - else if cost2 < cost1 && cost2 <= lcost[Y]
+     * - - lcost[Y] = cost2
+     * - - linsert[Y] = [X,B]
+     * - keep track of the Y associated with the smallest cost. Call this Ymin
+     * 
+     * if rc + lcosts[Ymin] <= MAX_COST
+     * - insert linsert[Ymin] into the cycle between A and B
+     * - rc = rc + lcost[Ymin]
+     * - set linsert[Ymin] to null 
+     * - mark Ymin as visited
+     * 
+     * else terminate: return cycle
+     * 
+     * Note that when Y is inserted, there are two new links to consider in the cycle
+     * 
+     * repeat Function with (A,B) = linsert[Ymin]
+     *
+     */
+function findCycle(startNodeI){
+
+    //cycle being made
+    var cycle = new Cycle();
+    
+    markVisited(startNodeI);
+    cycle.add(startNodeI);
+    cycle.add(startNodeI);
+
+    LOG && console.log("break2");
+    var Lcosts = Array(costMatrix[0].length).fill(Infinity);
+    var Linsert = Array(costMatrix[0].length).fill([startNodeI, startNodeI]);
+
+    var newNodeI = cycleExtendCandidates(Lcosts, Linsert, startNodeI, startNodeI, startNodeI);
+
+    LOG && console.log("jello");
+
+    //maximize nodes in the cycle
+    while(cycle.getCost() + Lcosts[newNodeI] <= MAX_COST && unvisited > 0){
+        LOG && cycle.logCycle();
+        cycle.insert(Linsert[newNodeI][0], newNodeI);
+        markVisited(newNodeI);
+        newNodeI = cycleExtendCandidates(Lcosts, Linsert, Linsert[newNodeI][0], newNodeI, Linsert[newNodeI][1]);
+    }
+
+    LOG && cycle.logCycle();
+
+    return cycle;
+
+}
+
+//helper for find cycle
+function cycleExtendCandidates(Lcosts, Linsert, AI, BI, CI){
+    //cycle goes A-B-C
+
+    var minI = 0;
+    var minCost = Infinity;
+    var costAXB;
+    var costBXC;
+
+    for (var XI = 0; XI < costMatrix[0].length; XI++){
+        if (!visited[XI]){
+            
+            //Lcosts[XI] = current minimum cost to add node X. 
+            //X would be added between Linsert[XI]
+            //costAXB = net cost of adding X between A and B
+            //costBXC = net cost of adding X between B and C
+             
+            costAXB = cost(AI, XI) + cost(XI, BI) - cost(AI, BI);
+            costBXC = cost(BI, XI) + cost(XI, CI) - cost(BI, CI);
+
+
+            //Lcosts[XI] = Math.min(Lcosts[XI], costAXB, costBXC);
+
+            if (costAXB <= Lcosts[XI] && costAXB <= costBXC){
+                // if adding X between A and B would incurr a lower net cost than adding X between some other nodes
+                // then we update Lcosts with that cost and Linsert with A and B
+                Lcosts[XI] = costAXB;
+                Linsert[XI] = [AI, BI];
+            } else if(costBXC <= Lcosts[XI] && costBXC <= costAXB) {
+                Lcosts[XI] = costBXC;
+                Linsert[XI] = [BI, CI];
+            }  
+
+            if (Lcosts[XI] <= minCost){
+                minI = XI;
+                minCost = Lcosts[XI];
+            }
+
+        }
+    }
+
+    return minI;
+
+}
+
+
+function indexSort(array){
+    var len = array.length;
+    var indices = new Array(array);
+    for (var i = 0; i < len; i++) indices[i] = i;
+    indices.sort(function (a, b) { return array[a] > array[b] ? -1 : array[a] < array[b] ? 1 : 0; });
+    //console.log(indices);
+    return indices;
+  }
+
+//findCycle(0);
+
+function totalCosts(){
+    var costSums = Array(costMatrix.length);
+
+    for (var i = 0; i < costMatrix.length; i++){
+        costSums[i] = costMatrix[i].reduce((partialSum, a) => partialSum + a, 0);
+    }
+    return costSums;
+}
+
+function createCycles(){
+    var cycles = []
+    var totalCost = 0;
+    var newCycle;
+
+    //produce an array of indices corresponding to decending order of row sums of the costMatrix
+    var indices = indexSort(totalCosts());
+    for (var nodeI = 0; nodeI < nodes.length; nodeI++){
+        if (!visited[indices[nodeI]]){
+            newCycle = findCycle(indices[nodeI]);
+            cycles.push(newCycle);
+            totalCost += newCycle.getCost();
+            //newCycle.logCycle();
+        }
+    }
+
+    console.log(totalCost);
+    return cycles
+}
+
+
+//TODO: order nodes by total cost (decending)
+
+
+//createCycles();
+
+
+
+
+
+
+function exp(cyclesMatrix){
+  app.get('/', (req, res) => {
+    res.write("hello there");
+    console.log("helo");
+    
+    //res.status(200).send('<h1>HELLO THERE</h1>');
+    //res.json(cyclesMatrix);
+
+    for (var i = 0; i < cyclesMatrix.length; i++){
+      res.write("\n");
+      res.write("cycle: \t");
+      res.write(cyclesMatrix[i].join("->"));
+    }
+    
+    res.end();
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+
+}
